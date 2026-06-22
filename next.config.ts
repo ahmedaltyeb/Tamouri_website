1b453b71067c699fa31e397b81dde6e8858f0c69
Your Name <your@email.com>
2026-06-22 14:13:34 +0400

    update

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export", // 👈 required for static site export
+
   images: {
+    unoptimized: true, // 👈 required (Next Image optimization won't work on static hosting)
+
     remotePatterns: [
       {
         protocol: "https",
